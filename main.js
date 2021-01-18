8bc719876907716c7152151eefb78ffb967a07b4
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-01-18 16:06:54 +0100

    Transition delay of span geerated element changed

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const lables = document.querySelectorAll('.form-controll label')
 lables.forEach(lable =>{
     lable.innerHTML = lable.innerText
         .split('')
-        .map((letter, index) => `<span style="transition-delay:${index * 150}ms">${letter}</span>`)
+        .map((letter, index) => `<span style="transition-delay:${index * 100}ms">${letter}</span>`)
         .join('')
 }) 
